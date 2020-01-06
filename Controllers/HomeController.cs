a76d3121e44e49de57704d6bbe872aa180640cdd
David Waldron <davidwaldron66@gmail.com>
2020-01-06 10:07:35 -0600

    Created the bones of a login and reg

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using ECommerce.Models;
+
+namespace ECommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext dbContext;
+        public HomeController(MyContext context)
+        {
+            dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
